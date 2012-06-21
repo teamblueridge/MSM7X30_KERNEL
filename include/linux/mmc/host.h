3a112f2e37fc887e04d3b245bb9c3a1c5b4ad516
AdiPat <adithemagnificent@gmail.com>
2012-06-21 21:46:05 +0530

    3.0.35

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -373,6 +373,7 @@ extern void mmc_request_done(struct mmc_host *, struct mmc_request *);
 static inline void mmc_signal_sdio_irq(struct mmc_host *host)
 {
 	host->ops->enable_sdio_irq(host, 0);
+	host->sdio_irq_pending = true;
 	wake_up_process(host->sdio_irq_thread);
 }
 
