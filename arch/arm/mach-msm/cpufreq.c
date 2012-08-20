557289a614b4745364126a2c26602a3333017538
Jmz <jmztaylor@gmail.com>
2012-08-20 11:01:48 -0700

    fixing bootup reboot

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -219,6 +219,8 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	init_completion(&cpu_work->complete);
 #endif
 
+        policy->min = 245760;
+        policy->max = 1024000;
 	return 0;
 }
 
