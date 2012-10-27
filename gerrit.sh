7104d991bfc1f1ef08b90f0194eb0d51d2c0032e
Simon Sickle <simonsimons34@gmail.com>
2012-10-26 19:59:26 -0700

    gerrit script for pushing to tbr gerrit

---
+++ gerrit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "WELCOME TO TBR GERRIT. Please have an account created"
+if [ -f ~/.ssh/tbr_username ]
+  then
+    echo "You've already been through setup. Lets continue!"
+  else
+    echo -n "what is your Gerrit username: "
+    read un
+    cat $un > ~/.ssh/tbr_username
+	echo "BE SURE YOUR SSH KEYS ARE MATCHED WITH GERRIT IN YOUR SETTINGS"
+fi
+un=`cat ~/.ssh/tbr_username`
+echo "Your username is $un to reconfigure, run this command (w/o quotes) 'rm -rf ~/.ssh/tbr_username'"
+echo -n "What is the project name: "
+read projectname
+echo -n "What branch are you pushing to: "
+read branch
+git push "ssh://$un@gerrit.teamblueridge.com:29418/$projectname" "HEAD:refs/for/$branch"
