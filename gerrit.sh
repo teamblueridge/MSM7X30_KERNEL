06c01fb97ccb49b6bf992fb3c8c9af96f29c87a9
Kyle Laker <10kylelaker@gmail.com>
2012-10-27 16:02:45 -0400

    Update gerrit script further

--- gerrit.sh
+++ gerrit.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-echo "WELCOME TO TBR GERRIT. Please have an account created"
 if [ -f ~/.ssh/tbr_username ]
   then
+    echo "Welcome back to Team BlueRidge Gerrit!"
     echo "You've already been through setup. Lets continue!"
   else
-    echo -n "what is your Gerrit username: "
-    read un
-    cat $un > ~/.ssh/tbr_username
+    echo "Welcome to Team BlueRidge Gerrit"
+    echo "You need to setup your account."
+    echo "If you don't have a Team BlueRidge Gerrit account, please go to http://gerrit.teamblueridge.com:8080 and create one."
+    echo -n "What is your Gerrit username: "
+    read un #un = Username
+    echo $un > ~/.ssh/tbr_username
 	echo "BE SURE YOUR SSH KEYS ARE MATCHED WITH GERRIT IN YOUR SETTINGS"
 fi
-un=`cat ~/.ssh/tbr_username`
+un=`cat ~/.ssh/tbr_username` # Redefines $un from tbr_username file (from setup)
 echo "Your username is $un to reconfigure, run this command (w/o quotes) 'rm -rf ~/.ssh/tbr_username'"
 echo -n "What is the project name: "
 read projectname
