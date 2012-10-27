7e3e390ad8044a1b8c973771ebf997369b74fbf3
Simon Sickle <simonsimons34@gmail.com>
2012-10-26 19:45:23 -0700

    add a script to allow easy gerrit pushes

---
+++ gerrit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "WELCOME TO TBR GERRIT. Please have an account created"
+if [ -f ~/.ssh/tbr_username ]
+  then
+    echo "You've already been through setup. Lets continue!"
+  else
+    echo -n "what is your Gerrit username: "
+    read un
+	touch ~/.ssh/tbr_username
+    cat $un > ~/.ssh/tbr_username
+	echo "BE SURE YOUR SSH KEYS ARE MATCHED WITH GERRIT IN YOUR SETTINGS"
+fi
+un=`cat ~/.ssh/tbr_username`
+echo "Your username is $un to reconfigure, run this command (w/o quotes) 'rm -rf ~/.ssh/tbr_username'"
+echo -n "What is the project name: "
+read projectname
+echo -n "What branch are you pushing to: "
+read branch
+echo -n "What is your commit message: "
+read commit
+git add .
+git commit -am `$commit`
+git push ssh://"$un"@gerrit.teamblueridge.com:29418/"$projectname" HEAD:refs/for/"$branch"
