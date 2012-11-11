bdfe60870b88a9d326631bb532f25f36918d4e62
Simon Sickle <simonsimons34@gmail.com>
2012-11-11 14:50:30 -0700

    CAF Drivers for Video
    Change-Id: I268c86bfbe5ae739a8d493d11bb3c00bc4821668

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -1,4 +1,4 @@
-/* arch/arm/mach-msm/include/mach/board.h
+W/* arch/arm/mach-msm/include/mach/board.h
  *
  * Copyright (C) 2007 Google, Inc.
  * Copyright (c) 2008-2012, Code Aurora Forum. All rights reserved.
@@ -32,6 +32,7 @@
 #endif
 #include <mach/msm_bus.h>
 
+#define PANEL_NAME_MAX_LEN 50
 struct msm_camera_io_ext {
 	uint32_t mdcphy;
 	uint32_t mdcsz;
@@ -445,6 +446,8 @@ enum msm_mdp_hw_revision {
 	MDP_REV_40,
 	MDP_REV_41,
 	MDP_REV_42,
+	MDP_REV_43,
+	MDP_REV_44,
 };
 
 struct msm_panel_common_pdata {
@@ -483,6 +486,7 @@ struct msm_panel_common_pdata {
 	int fpga_3d_config_addr;
 	struct gamma_curvy *abl_gamma_tbl;
 #endif
+	char cont_splash_enabled;
 };
 
 struct lcdc_platform_data {
@@ -548,6 +552,8 @@ struct msm_fb_platform_data {
 	uint32_t width;
 	uint32_t height;
 	bool     is_3d_panel;
+        char prim_panel_name[PANEL_NAME_MAX_LEN];
+        char ext_panel_name[PANEL_NAME_MAX_LEN];
 };
 
 #ifdef CONFIG_FB_MSM8960
